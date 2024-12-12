990abf4526e38c6c2ff86477627792f18ba1ca47
Anna Rider <3461836+annarider@users.noreply.github.com>
2024-12-11 20:58:57 -0700

    Update LPD info in client.js data

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,21 +1,21 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
-    "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "country": "US",
-        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
-    },
-    "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
-    },
-    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+  name: "Lucky Pixels Design",
+  email: "anna@luckypixelsdesign.com",
+  phoneForTel: "303-335-9629",
+  phoneFormatted: "+1 (303) 335-9629",
+  address: {
+    lineOne: "2525 Arapahoe Ave",
+    lineTwo: "Suite E4-240",
+    city: "Boulder",
+    state: "CO",
+    zip: "80302",
+    country: "US",
+    mapLink: "https://maps.app.goo.gl/A5YbxUzrSyqR4zrY6",
+  },
+  socials: {
+    facebook: "https://www.facebook.com/",
+    instagram: "https://www.instagram.com/",
+  },
+  //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+  domain: "https://luckypixelsdesign.com",
 };
